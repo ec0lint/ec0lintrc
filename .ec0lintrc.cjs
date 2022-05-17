c33f59331977ce6e5c8a4daa39461a0a09a7d8ca
malwinq <malwina.kubas@gmail.com>
2022-05-17 22:13:17 +0200

    fix linter config

--- .ec0lintrc.cjs
+++ .ec0lintrc.cjs
@@ -5,8 +5,13 @@ module.exports = {
     extends: [
         "ec0lint"
     ],
+    env: {
+        es6: true,
+        node: true
+    },
     parserOptions: {
-        ecmaVersion: 2020
+        ecmaVersion: 2020,
+        sourceType: "module"
     },
 
     overrides: [
