b0505bbedbf627b9034c7e5ae5a6c2f6c9e78d05
malwinq <malwina.kubas@gmail.com>
2022-05-28 12:35:58 +0200

    upgrade config

--- .ec0lintrc.cjs
+++ .ec0lintrc.cjs
@@ -18,12 +18,7 @@ module.exports = {
         {
             files: ["tests/**/*"],
             env: { mocha: true },
-            rules: {
-                "no-restricted-syntax": ["error", {
-                    selector: "CallExpression[callee.object.name='assert'][callee.property.name='doesNotThrow']",
-                    message: "`assert.doesNotThrow()` should be replaced with a comment next to the code."
-                }]
-            }
+            rules: {  }
         }
     ]
 };
